1190c69adb37ec99a4679d232e29191bfe2da84a
sacha-renault <sacha35340@gmail.com>
2024-10-18 11:58:29 +0200

    fix: github credentials ?

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -18,7 +18,6 @@ public:
     void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound*, int currentPitchWheelPosition) override;
     void stopNote(float velocity, bool allowTailOff) override;
     void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
-
     void pitchWheelMoved(int newPitchWheelValue) override;
     void controllerMoved(int controllerNumber, int newValue) override;
 
@@ -29,7 +28,6 @@ public:
     float frequency;
     float phase = 0.0f;
     int position = 0;
-
 };
 
 
